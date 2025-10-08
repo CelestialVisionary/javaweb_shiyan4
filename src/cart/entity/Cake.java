45daf0ed68c8b7a54aacbccbbf602f3c4910b085
CelestialVisionary <zilvdebao@outlook.com>
2025-10-08 16:28:54 +0800

    在Cake类中添加price属性

--- src/cart/entity/Cake.java
+++ src/cart/entity/Cake.java
@@ -6,24 +6,44 @@ public class Cake {
     //声明私有变量，作为Cake的属性
     private String id;
     private String name;
+    private double price;
+    
     public Cake() {     //无参数构造方法
     }
+    
     public Cake(String id, String name) {    //有参数构造方法
         this.id = id;
         this.name = name;
+    }
+    
+    public Cake(String id, String name, double price) {    //有参数构造方法，包含price
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
     //获取Getter和修改Setter属性的相关方法
     public String getId() {
         return id;
     }
+    
     public void setId(String id) {
         this.id = id;
     }
+    
     public String getName() {
         return name;
     }
+    
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
